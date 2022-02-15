4171e52dfd68bbd86029d8c7afbd0995fe3e065c
SunLee128 <sunlee128@gmail.com>
2022-02-15 19:47:54 +1100

    Update style

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,4 @@ class Item < ApplicationRecord
   validates :description, presence: true
   after_create_commit { broadcast_append_to "items" }
   after_destroy_commit { broadcast_remove_to "items" }
-  
 end
