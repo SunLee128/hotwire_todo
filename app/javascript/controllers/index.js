62e553e316c5356658b6f55925db306ff392b09e
Zendesk-NathanBellette <nathan.bellette@zendesk.com>
2022-02-16 23:44:46 +1100

    Add stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,4 +5,7 @@
 import { application } from "./application"
 
 import HelloController from "./hello_controller"
+import ItemsController from "./items_controller"
+
 application.register("hello", HelloController)
+application.register("items", ItemsController)
---
+++ app/javascript/controllers/items_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+    checkboxClicked () {
+        console.log('selected checkboxes: ', this.selectedCheckboxes);
+    }
+
+    get selectedCheckboxes() {
+        return this.checkboxes.filter((c) => c.checked);
+    }
+
+    get checkboxes() {
+        return new Array(...this.element.querySelectorAll("input[type=checkbox]"));
+    }
+}
