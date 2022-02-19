2a72995fb75df98cc534688b599f5eea268dcd8f
SunLee128 <sunlee128@gmail.com>
2022-02-19 21:04:39 +1100

    Update total counts

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,14 +25,14 @@ class ItemsController < ApplicationController
   # POST /items or /items.json
   def create
     @item = Item.new(item_params)
+    @items = Item.all
 
     respond_to do |format|
       if @item.save
-        format.html { render @item, notice: "Item was successfully created." }
-        # format.json { render :show, status: :created, location: @item }
+        format.turbo_stream { render :create }
+        format.html { redirect_to items_url }
       else
-        # format.html { render :new, status: :unprocessable_entity }
-        # format.json { render json: @item.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity
       end
     end
   end
@@ -51,21 +51,22 @@ class ItemsController < ApplicationController
   # DELETE /items/1 or /items/1.json
   def destroy
     @item.destroy
+    @items = Item.all
 
     respond_to do |format|
-      format.html { render @item, notice: "Item was successfully destroyed." }
-      format.json { head :no_content }
+      format.turbo_stream { render :destroy }
+      format.html { redirect_to items_url }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_item
-      @item = Item.find(params[:id])
-    end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def item_params
-      params.require(:item).permit(:description, :completed)
-    end
+  # Only allow a list of trusted parameters through.
+  def item_params
+    params.require(:item).permit(:description, :completed)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,3 @@
 class Item < ApplicationRecord
   validates :description, presence: true
-  after_create_commit { broadcast_append_to "items" }
-  after_destroy_commit { broadcast_remove_to "items" }
 end
